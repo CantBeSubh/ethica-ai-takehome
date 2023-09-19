1b142843783a3fc495978be8f1091f224ff4abbd
Subhranshu Pati <subhranshupati0412@gmail.com>
2023-09-20 03:07:15 +0530

    add: Add Review button refactor: API

--- components/input-modal.tsx
+++ components/input-modal.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react"
 import {
     AlertDialog,
     AlertDialogAction,
@@ -13,6 +15,29 @@ import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 
 export function InputModal() {
+    const handleClick = async (e: React.MouseEvent) => {
+        e.preventDefault();
+        // console.log(input);
+        try {
+            const response = await fetch("/api/analyse", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ input }),
+            });
+            const data = await response.json();
+            const sentiment = data.sentiment;
+            console.log(sentiment);
+        }
+        catch (err) {
+            console.log(err);
+        }
+        finally {
+            setInput("")
+        }
+    }
+    const [input, setInput] = useState("");
     return (
         <AlertDialog>
             <AlertDialogTrigger asChild>
@@ -23,12 +48,16 @@ export function InputModal() {
                     <AlertDialogTitle>Add a Review?</AlertDialogTitle>
                     <AlertDialogDescription>
                         {/* Enter a review for this movie. */}
-                        <Input placeholder="Enter your review here" />
+                        <Input
+                            placeholder="Enter your review here"
+                            value={input}
+                            onChange={(e) => setInput(e.target.value)}
+                        />
                     </AlertDialogDescription>
                 </AlertDialogHeader>
                 <AlertDialogFooter>
                     <AlertDialogCancel>Cancel</AlertDialogCancel>
-                    <AlertDialogAction>Analyse</AlertDialogAction>
+                    <AlertDialogAction onClick={(e) => handleClick(e)}>Analyse</AlertDialogAction>
                 </AlertDialogFooter>
             </AlertDialogContent>
         </AlertDialog>
