94d5b190f722bb0c9cb434852b512723067dfb62
Subhranshu Pati <subhranshupati0412@gmail.com>
2023-09-20 02:51:19 +0530

    add: API Endpoint

---
+++ app/api/get-sentiment/route.ts
@@ -0,0 +1,37 @@
+// async function query(data) {
+// 	const response = await fetch(
+// 		"https://api-inference.huggingface.co/models/distilbert-base-uncased-finetuned-sst-2-english",
+// 		{
+// 			headers: { Authorization: "Bearer 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" },
+// 			method: "POST",
+// 			body: JSON.stringify(data),
+// 		}
+// 	);
+// 	const result = await response.json();
+// 	return result;
+// }
+
+// query({"inputs": "I like you. I love you"}).then((response) => {
+// 	console.log(JSON.stringify(response));
+// });
+
+import { NextResponse } from 'next/server'
+export function GET(req: Request) {
+    return NextResponse.json({ "Hello": "World", "Status": "OK" }, { status: 200 })
+}
+
+export async function POST(req: Request) {
+    const { input } = await req.json()
+    const response = await fetch(
+        "https://api-inference.huggingface.co/models/distilbert-base-uncased-finetuned-sst-2-english",
+        {
+            headers: { Authorization: "Bearer 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" },
+            method: "POST",
+            body: JSON.stringify(input),
+        }
+    );
+    const results = await response.json();
+    const result = results[0] as { label: string, score: number }[]
+    const sentiment = result.sort((a, b) => b.score - a.score)[0].label || "NEUTRAL"
+    return NextResponse.json({ sentiment }, { status: 200 })
+}
