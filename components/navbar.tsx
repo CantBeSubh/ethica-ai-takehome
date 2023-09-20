2dcf8a1b18706fc827bf230f185ac8f8e7d318ec
Subhranshu Pati <subhranshupati0412@gmail.com>
2023-09-20 14:54:28 +0530

    refactor

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,9 @@
-import { ModeToggle } from "./mode-toggle"
+import { ModeToggle } from "@/components/mode-toggle"
 
 const Navbar = () => {
     return (
-        <nav className="flex justify-center space-x-4 items-center border-b-2 p-4">
-            <h1 className="text-5xl font-bold">MOVIE<span className="text-orange-600">REVIEWS</span></h1>
+        <nav className="flex items-center justify-center p-4 space-x-4 border-b-2">
+            <h1 className="text-5xl font-bold">MOVIE <span className="text-orange-600">REVIEWS</span></h1>
             <ModeToggle />
         </nav>
     )
