71f0d3c1b1143afeae1199c80fe378a49ce54fbe
Subhranshu Pati <subhranshupati0412@gmail.com>
2023-09-20 12:07:27 +0530

    add: LocalStorage presistance

--- state/use-reviews.ts
+++ state/use-reviews.ts
@@ -1,6 +1,7 @@
 'use client'
 import { Review } from '@/components/reviews/columns';
 import { create } from 'zustand';
+import { persist, createJSONStorage } from "zustand/middleware";
 
 interface ReviewsStore {
     reviews: Review[];
@@ -10,36 +11,41 @@ interface ReviewsStore {
     deleteReview: (iat: Date) => void;
 }
 
-export const useReviews = create<ReviewsStore>((set) => ({
-    reviews: [],
-    addReview: (review) =>
-        set((state) => ({
-            reviews: [...state.reviews, review],
-        })),
-    upvoteReview: (iat) =>
-        set((state) => ({
-            reviews: state.reviews.map((review) =>
-                review.iat === iat
-                    ? {
-                        ...review,
-                        up: review.up + 1,
-                    }
-                    : review
-            ),
-        })),
-    downvoteReview: (iat) =>
-        set((state) => ({
-            reviews: state.reviews.map((review) =>
-                review.iat === iat
-                    ? {
-                        ...review,
-                        down: review.down + 1,
-                    }
-                    : review
-            ),
-        })),
-    deleteReview: (iat) =>
-        set((state) => ({
-            reviews: state.reviews.filter((review) => review.iat !== iat),
-        })),
-}));
+export const useReviews = create(
+    persist<ReviewsStore>(
+        (set) => ({
+            reviews: [],
+            addReview: (review) =>
+                set((state) => ({
+                    reviews: [...state.reviews, review],
+                })),
+            upvoteReview: (iat) =>
+                set((state) => ({
+                    reviews: state.reviews.map((review) =>
+                        review.iat === iat
+                            ? {
+                                ...review,
+                                up: review.up + 1,
+                            }
+                            : review
+                    ),
+                })),
+            downvoteReview: (iat) =>
+                set((state) => ({
+                    reviews: state.reviews.map((review) =>
+                        review.iat === iat
+                            ? {
+                                ...review,
+                                down: review.down + 1,
+                            }
+                            : review
+                    ),
+                })),
+            deleteReview: (iat) =>
+                set((state) => ({
+                    reviews: state.reviews.filter((review) => review.iat !== iat),
+                })),
+        }), {
+        name: "reviews-storage",
+        storage: createJSONStorage(() => localStorage),
+    }));
