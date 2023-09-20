1266cdc011e11ce64a759472ceefd12f6db493df
Subhranshu Pati <subhranshupati0412@gmail.com>
2023-09-20 17:17:07 +0530

    Comments

--- app/api/analyse/route.ts
+++ app/api/analyse/route.ts
@@ -1,15 +1,18 @@
 import { NextResponse } from 'next/server'
 
 export function GET() {
+    //A general GET request to the route will return a 200 status code and a JSON object with a "Hello" and "World" key-value pair.
     return NextResponse.json({ "Hello": "World", "Status": "OK" }, { status: 200 })
 }
 
 export async function POST(req: Request) {
     const { input } = await req.json()
+    // If the input is missing, return a 400 status code and a JSON object with an "error" key-value pair.
     if (!input) {
         return NextResponse.json({ error: "Missing input" }, { status: 400 })
     }
     try {
+        // Make a POST request to the Hugging Face API with the input and the API key.
         const response = await fetch(
             "https://api-inference.huggingface.co/models/distilbert-base-uncased-finetuned-sst-2-english",
             {
@@ -18,6 +21,7 @@ export async function POST(req: Request) {
                 body: JSON.stringify(input),
             }
         );
+        // Response would be a Array of Array of Objects. We sort the array based on the score and return the label with the highest score.
         const results = await response.json();
         const result = results[0] as { label: string, score: number }[]
         const sentiment = result.sort((a, b) => b.score - a.score)[0].label || "NEUTRAL"
